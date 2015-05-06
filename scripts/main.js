24294d4581ff44bddf0e1253f721611472c55c30
Palmer <pzpaul2002@yahoo.com>
2015-05-06 13:15:43 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
-document.addEventListener("touchstart", function (event) {
+var touchX, touchY;
+
+var touchHandler = function (event) {
   event.preventDefault();
-});
-document.addEventListener("touchmove", function (event) {
-  event.preventDefault();
-});
+  touchX = e.touches[0].offsetX;
+  touchY = e.touches[0].offsetY;
+}
+
+document.addEventListener("touchstart", touchHandler);
+document.addEventListener("touchmove", touchHandler);
